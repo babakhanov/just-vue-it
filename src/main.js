193132c8c1b054558f50689b2feb7e8e00da74c4
Alex Babakhanov <babakhanov1@gmail.com>
2017-09-21 11:58:11 +0300

    decrease size of js entry

--- src/main.js
+++ src/main.js
@@ -4,7 +4,10 @@ require.context("reveal.js/lib/font/league-gothic", true, /\.(eot|ttf|svg|woff)$
 require('./stylesheets/main.scss');
 
 import Reveal from 'reveal.js';
-import hljs from 'highlight.js';
+import hljs from 'highlight.js/lib/highlight.js';
+
+hljs.registerLanguage('javascript', require('highlight.js/lib/languages/javascript'));
+hljs.registerLanguage('shell', require('highlight.js/lib/languages/shell'));
 
 Reveal.initialize({
   history: true,
