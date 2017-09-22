23cb08076cbeffc54cff895ff5dc075572ec2623
Alex Babakhanov <babakhanov1@gmail.com>
2017-09-22 11:43:52 +0300

    cli

--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ import hljs from 'highlight.js/lib/highlight.js';
 
 hljs.registerLanguage('javascript', require('highlight.js/lib/languages/javascript'));
 hljs.registerLanguage('shell', require('highlight.js/lib/languages/shell'));
+hljs.registerLanguage('xml', require('highlight.js/lib/languages/xml'));
 
 Reveal.initialize({
   history: true,
